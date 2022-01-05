5449b25e871727f5ce09b640140876a766b34187
Marlon Chhour <bane6459@gmail.com>
2022-01-05 17:57:45 +1300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,53 +16,64 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {   
-        // removes all rows from the specified table
+        // only need if we need to refresh database multiple times throughout development
         User::truncate();
         Post::truncate();
         Category::truncate();
 
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
+        $user = User::factory()->create([
+            'name' => 'John Doe'
         ]);
 
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family'
+        Post::factory(5)->create([
+            'user_id' => $user->id
         ]);
 
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
+        // Post::factory(10)->create(); // creates user, category and post 
 
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $family->id,
-            'title'=>'My Family Post',
-            'slug'=>'my-family-post',
-            'excerpt'=>'Lorem ipsum dolor sit amet.',
-            'body'=>'<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Felis donec et odio pellentesque diam volutpat commodo sed.</p>'
-        ]);
+        // $user = User::factory()->create();
 
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title'=>'My Work Post',
-            'slug'=>'my-work-post',
-            'excerpt'=>'Lorem ipsum dolor sit amet.',
-            'body'=>'<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Felis donec et odio pellentesque diam volutpat commodo sed.</p>'
-        ]);
 
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $personal->id,
-            'title'=>'My Personal Post',
-            'slug'=>'my-personal-post',
-            'excerpt'=>'Lorem ipsum dolor sit amet.',
-            'body'=>'<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Felis donec et odio pellentesque diam volutpat commodo sed.</p>'
-        ]);
+        // $personal = Category::create([
+        //     'name' => 'Personal',
+        //     'slug' => 'personal'
+        // ]);
+
+        // $family = Category::create([
+        //     'name' => 'Family',
+        //     'slug' => 'family'
+        // ]);
+
+        // $work = Category::create([
+        //     'name' => 'Work',
+        //     'slug' => 'work'
+        // ]);
+
+        // Post::create([
+        //     'user_id' => $user->id,
+        //     'category_id' => $family->id,
+        //     'title'=>'My Family Post',
+        //     'slug'=>'my-family-post',
+        //     'excerpt'=>'Lorem ipsum dolor sit amet.',
+        //     'body'=>'<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Felis donec et odio pellentesque diam volutpat commodo sed.</p>'
+        // ]);
+
+        // Post::create([
+        //     'user_id' => $user->id,
+        //     'category_id' => $work->id,
+        //     'title'=>'My Work Post',
+        //     'slug'=>'my-work-post',
+        //     'excerpt'=>'Lorem ipsum dolor sit amet.',
+        //     'body'=>'<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Felis donec et odio pellentesque diam volutpat commodo sed.</p>'
+        // ]);
+
+        // Post::create([
+        //     'user_id' => $user->id,
+        //     'category_id' => $personal->id,
+        //     'title'=>'My Personal Post',
+        //     'slug'=>'my-personal-post',
+        //     'excerpt'=>'Lorem ipsum dolor sit amet.',
+        //     'body'=>'<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Felis donec et odio pellentesque diam volutpat commodo sed.</p>'
+        // ]);
     }
 }
